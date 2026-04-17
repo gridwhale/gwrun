ac44b27d5a027902ec3802b9426be433b359ae46
George Moromisato <public@neurohack.com>
2026-04-17 15:01:56 -0700

    Format tools list text output

--- src/commands.c
+++ src/commands.c
@@ -9,6 +9,240 @@ static int wants_json(const GwOptions *opts)
 	return strcmp(opts->output, "json") == 0;
 }
 
+static const char *find_range(const char *start, const char *end, const char *needle)
+{
+	size_t needle_len = strlen(needle);
+	const char *p;
+
+	if (needle_len == 0) {
+		return start;
+	}
+
+	for (p = start; p && p + needle_len <= end; p++) {
+		if (memcmp(p, needle, needle_len) == 0) {
+			return p;
+		}
+	}
+	return NULL;
+}
+
+static const char *find_last_range(const char *start, const char *end, const char *needle)
+{
+	const char *p = start;
+	const char *last = NULL;
+
+	while ((p = find_range(p, end, needle)) != NULL) {
+		last = p;
+		p++;
+	}
+	return last;
+}
+
+static const char *find_matching_object_end(const char *object_start)
+{
+	const char *p;
+	int depth = 0;
+	int in_string = 0;
+	int escape = 0;
+
+	for (p = object_start; *p; p++) {
+		if (in_string) {
+			if (escape) {
+				escape = 0;
+			} else if (*p == '\\') {
+				escape = 1;
+			} else if (*p == '"') {
+				in_string = 0;
+			}
+			continue;
+		}
+
+		if (*p == '"') {
+			in_string = 1;
+		} else if (*p == '{') {
+			depth++;
+		} else if (*p == '}') {
+			depth--;
+			if (depth == 0) {
+				return p + 1;
+			}
+		}
+	}
+	return NULL;
+}
+
+static char *json_string_dup(const char *quote, const char **end_out)
+{
+	GwBuffer out;
+	const char *p;
+	char ch;
+
+	if (!quote || *quote != '"') {
+		return NULL;
+	}
+
+	buffer_init(&out);
+	for (p = quote + 1; *p; p++) {
+		if (*p == '"') {
+			if (end_out) {
+				*end_out = p + 1;
+			}
+			if (!out.data) {
+				if (!buffer_append_cstr(&out, "")) {
+					return NULL;
+				}
+			}
+			return out.data;
+		}
+		if (*p == '\\') {
+			p++;
+			if (!*p) {
+				break;
+			}
+			switch (*p) {
+			case '"': ch = '"'; break;
+			case '\\': ch = '\\'; break;
+			case '/': ch = '/'; break;
+			case 'b': ch = '\b'; break;
+			case 'f': ch = '\f'; break;
+			case 'n': ch = '\n'; break;
+			case 'r': ch = '\r'; break;
+			case 't': ch = '\t'; break;
+			case 'u':
+				ch = '?';
+				if (p[1] && p[2] && p[3] && p[4]) {
+					p += 4;
+				}
+				break;
+			default:
+				ch = *p;
+				break;
+			}
+		} else {
+			ch = *p;
+		}
+		if (!buffer_append(&out, &ch, 1)) {
+			buffer_free(&out);
+			return NULL;
+		}
+	}
+
+	buffer_free(&out);
+	return NULL;
+}
+
+static void print_indented_text(const char *text, const char *indent)
+{
+	const char *p;
+
+	if (!text || !text[0]) {
+		return;
+	}
+
+	printf("%s", indent);
+	for (p = text; *p; p++) {
+		putchar(*p);
+		if (*p == '\n' && p[1]) {
+			printf("%s", indent);
+		}
+	}
+	if (p > text && p[-1] != '\n') {
+		putchar('\n');
+	}
+}
+
+static int print_tools_list_text(const char *json)
+{
+	const char *tools = strstr(json, "\"tools\"");
+	const char *scan;
+	int count = 0;
+
+	printf("Tools:\n");
+	if (!tools) {
+		printf("  (none)\n");
+		return 0;
+	}
+
+	scan = strchr(tools, '[');
+	if (!scan) {
+		printf("  (none)\n");
+		return 0;
+	}
+	scan++;
+
+	while (*scan) {
+		const char *object_start;
+		const char *object_end;
+		const char *name_key;
+		const char *name_colon;
+		const char *name_quote;
+		const char *name_end = NULL;
+		const char *desc_key;
+		char *name;
+		char *desc = NULL;
+
+		while (*scan && *scan != '{' && *scan != ']') {
+			scan++;
+		}
+		if (*scan == ']') {
+			break;
+		}
+		if (*scan != '{') {
+			break;
+		}
+
+		object_start = scan;
+		object_end = find_matching_object_end(object_start);
+		if (!object_end) {
+			break;
+		}
+
+		name_key = find_last_range(object_start, object_end, "\"name\"");
+		if (!name_key) {
+			scan = object_end;
+			continue;
+		}
+		name_colon = strchr(name_key, ':');
+		if (!name_colon) {
+			break;
+		}
+		name_quote = strchr(name_colon, '"');
+		if (!name_quote) {
+			break;
+		}
+
+		name = json_string_dup(name_quote, &name_end);
+		if (!name) {
+			break;
+		}
+
+		desc_key = find_range(object_start, object_end, "\"description\"");
+		if (desc_key) {
+			const char *desc_colon = strchr(desc_key, ':');
+			const char *desc_quote = desc_colon ? strchr(desc_colon, '"') : NULL;
+			if (desc_quote && desc_quote < object_end) {
+				desc = json_string_dup(desc_quote, NULL);
+			}
+		}
+
+		printf("  %s\n", name);
+		if (desc && desc[0]) {
+			print_indented_text(desc, "    ");
+		}
+		free(name);
+		free(desc);
+
+		count++;
+		(void)name_end;
+		scan = object_end;
+	}
+
+	if (count == 0) {
+		printf("  (none)\n");
+	}
+	return count;
+}
+
 static int print_remote_result(const GwOptions *opts, const char *command, GwHttpResponse *res, long duration_ms)
 {
 	if (res->status >= 200 && res->status < 300) {
@@ -109,8 +343,18 @@ int command_tools_list(const GwOptions *opts)
 	long start = monotonic_ms();
 	int ok = mcp_call(opts, "tools/list", "{}", &res);
 	long duration = monotonic_ms() - start;
-	int code = print_remote_result(opts, "tools.list", &res, duration);
+	int code;
 	(void)ok;
+
+	if (res.status >= 200 && res.status < 300 && !wants_json(opts)) {
+		printf("Server: %s\n", opts->server);
+		printf("Duration: %ld ms\n", duration);
+		print_tools_list_text(res.body.data ? res.body.data : "");
+		code = 0;
+	} else {
+		code = print_remote_result(opts, "tools.list", &res, duration);
+	}
+
 	http_response_free(&res);
 	return code;
 }
